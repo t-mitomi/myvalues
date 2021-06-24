76c9fbc219b6a87ca0b306dbc2214fd1f88cc7a5
gittm <tsstest7@gmail.com>
2021-06-24 23:11:08 +0900

    values100 db更新、page4

--- static/js/common.js
+++ static/js/common.js
@@ -2,4 +2,23 @@ $(function(){
     $('a').on('click', function() {
       return false;
     });
-  });
+    // jQueryでテーブルの行を操作する（移動・削除・追加） https://qiita.com/zinbe/items/f5be42aa47d0960c7195
+    $('.rowup').click(function() {
+        
+        let $row = $(this).closest("tr");
+        let $row_prev = $row.prev("tr");
+        if($row.prev.length) {
+            $row.insertBefore($row_prev);
+        }
+
+    });
+    $('.rowdown').click(function() {
+
+        let $row = $(this).closest("tr");
+        let $row_next = $row.next("tr");
+        if($row_next.length) {
+            $row.insertAfter($row_next);
+        }
+
+    });
+});
