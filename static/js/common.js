11a8c62462bb1a39c197596dcdb15681eecca5d0
gittm <tsstest7@gmail.com>
2021-06-30 14:11:02 +0900

    page2+,page3+,page4++,app 水曜日2時

--- static/js/common.js
+++ static/js/common.js
@@ -1,18 +1,17 @@
 $(function(){
 
-// テーブル行のドラッグ＆ドロップ【JavaScript】
+    // テーブル行のドラッグ＆ドロップ【JavaScript】
     $('#sortdata').sortable();
 
-//テーブル行入れ替え後のセル値を更新
-// sortstopイベントをバインド
-$('#sortdata').bind('sortstop',function(){
-    // 番号を設定している要素に対しループ処理
-    $(this).find('[name="num_data"]').each(function(idx){
-      // タグ内に通し番号を設定（idxは0始まりなので+1する）
-    $(this).html(idx+1);
+    //テーブル行入れ替え後のセル値を更新
+    // sortstopイベントをバインド
+    $('#sortdata').bind('sortstop',function(){
+        // 番号を設定している要素に対しループ処理
+        $(this).find('[name="num_data"]').each(function(idx){
+        // タグ内に通し番号を設定（idxは0始まりなので+1する）
+        $(this).html(idx+1);
+        });
     });
-    });
-
 
     $('a').on('click', function() {
     return false;
@@ -35,58 +34,4 @@ $('#sortdata').bind('sortstop',function(){
             $row.insertAfter($row_next);
         }
     });
-    $("#selected").prop('disabled', true);
-    $('input[type="radio"]').on('change',function(){
-        // 複数要素に同じ処理 $('セレクタ').each(function(index, element){ 〜 });
-        var select_count=0;
-        $('input:radio:checked').each(function(idx,elem){
-            if(parseInt(elem.value)>0){
-                select_count+=1;
-            }
-            if(select_count>=10){
-                $("#selected").prop('disabled', false);
-            }else{
-                $("#selected").prop('disabled', true);
-            }
-            console.log(select_count+" 件選択中 " );
-            $("#msgarea").text(select_count+" 件選択中 ")
-        });
-    });
-    if($("tr").length!=11){
-        $(".select10").prop('disabled', true);
-    }
-    $('input[type="radio"]').on('change',function(){
-        // 複数要素に同じ処理 $('セレクタ').each(function(index, element){ 〜 });
-        var select_count=0;
-        $('input:radio:checked').each(function(idx,elem){
-            if(parseInt(elem.value)>0){
-                select_count+=1;
-            }
-            if(select_count==10){
-                $(".select10").prop('disabled', false);
-            }else{
-                $(".select10").prop('disabled', true);
-            }
-            console.log(select_count+" 件選択中 " );
-            $("#msgarea").text(select_count+" 件選択中 ")
-        });
-    });
-    $(".ranking_decision").prop('disabled', false);
-    $('input[type="radio"]').on('change',function(){
-        // 複数要素に同じ処理 $('セレクタ').each(function(index, element){ 〜 });
-        var select_count=0;
-        $('input:radio:checked').each(function(idx,elem){
-            if(parseInt(elem.value)>0){
-                select_count+=1;
-            }
-            if(select_count==10){
-                $(".ranking_decision").prop('disabled', false);
-            }else{
-                $(".ranking_decision").prop('disabled', true);
-            }
-            console.log(select_count+" 件選択中 " );
-            $("#msgarea").text(select_count+" 件選択中 ")
-        });
-    });
-
 });
